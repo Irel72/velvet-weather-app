e615eab07d9ea202bc6d8eab7a47daed32c7bf7a
Irel72 <117178371+Irel72@users.noreply.github.com>
2023-01-30 16:21:00 -0800

    Added the forecast HTML and CSS

--- src/app.js
+++ src/app.js
@@ -31,16 +31,17 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = documeny.querySelector("#icon");
 
+celciusTemperature = response.data.main.temp
 
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute(
-    "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}
-    @2x.png`);
+    "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
@@ -63,6 +64,7 @@ function displayFahrenheitTemperature(event) {
 
     celsiusLink.classList.remove("active");
     fahrenheitLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
     let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
@@ -70,8 +72,7 @@ function displayFahrenheitTemperature(event) {
 function displayCelsiusTemperature(event) {
     event.preventDefault();
     celsiusLink.celsiusList.add("active");
-    fahrenheitLink.celsiusList.remove("active");
-    let temperatureElement = document.querySelector("temperature");
+    let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(celciusTemperature);
 }
 
